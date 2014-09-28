0f81ffe5155208da9ad66e30bc95123de6a006a9
Daniel Hum <dan@oneuphero.com>
2014-09-28 19:11:01 +0800

    initial commit

---
+++ app/src/main/java/com/oneuphero/betternotifications/NLService.java
@@ -0,0 +1,20 @@
+package com.oneuphero.betternotifications;
+
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+/**
+ * Created by Serenity on 28/9/14.
+ */
+public class NLService extends NotificationListenerService {
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+
+    }
+}
