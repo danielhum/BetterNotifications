33ec31b853d59bd2bebce685d8cb87d356d6ec36
Daniel Hum <dan@oneuphero.com>
2015-02-08 17:38:11 +0800

    some refactoring

--- app/src/main/java/com/oneuphero/betternotifications/NLService.java
+++ app/src/main/java/com/oneuphero/betternotifications/NLService.java
@@ -41,7 +41,7 @@ public class NLService extends NotificationListenerService {
                 Notification.Builder nBuilder = new Notification.Builder(context);
                 nBuilder.setContentTitle("Better Notifications");
                 nBuilder.setContentText("starting service...");
-                nBuilder.setContentText("starting service...");
+                nBuilder.setTicker("starting service...");
                 nBuilder.setSmallIcon(R.drawable.ic_launcher);
                 nBuilder.setAutoCancel(true);
                 nManager.notify(TAG, NOTIFICATION_INITIAL, nBuilder.build());
@@ -50,45 +50,51 @@ public class NLService extends NotificationListenerService {
     }
 
     public void onNotificationPosted(StatusBarNotification sbn) {
-        Log.i(TAG, "Posted ID: " + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
+        Log.i(TAG, "Posted #" + sbn.getId() + " " + sbn.getNotification().tickerText + " " + sbn.getPackageName());
         //TODO: this is throwing null error?
         if(sbn.getTag() != null && sbn.getTag().equals(TAG) && sbn.getId() == NOTIFICATION_INITIAL) {
             cancelNotification(sbn.getPackageName(), TAG, NOTIFICATION_INITIAL);
             Log.i(TAG, "dismissed notification");
+        } else {
+            storeStatusBarNotification(sbn);
         }
         if (!mCollectedAllNotifications) {
             mCollectedAllNotifications = true;
             StatusBarNotification[] sbns = getActiveNotifications();
             for (StatusBarNotification activeSbn : sbns) {
                 Log.i(TAG, "NOTIFICATION: " + activeSbn.getNotification().tickerText);
-                Notification n = activeSbn.getNotification();
-                int notificationId = activeSbn.getId();
-                String packageName = activeSbn.getPackageName();
-                try {
-                    Log.d(TAG, "querying for: " + String.valueOf(notificationId) + " " + packageName);
-                    List<StoredNotification> notifications = getDao().queryBuilder().where()
-                                                                       .eq("notification_id", notificationId)
-                                                                       .and()
-                                                                       .eq("package_name", packageName).query();
-                    if (notifications.isEmpty()) {
-                        String title = n.tickerText == null ? null : n.tickerText.toString();
-                        getDao().create(new StoredNotification(notificationId, packageName, title, null));
-                        Log.d(TAG, "stored notification! [" + notificationId + "] " + title);
-                    } else {
-                        Log.d(TAG, "found notifications: ");
-                        for(StoredNotification sn : notifications) {
-                            Log.d(TAG, sn.toString());
-                        }
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+                storeStatusBarNotification(activeSbn);
             }
         }
     }
 
+    private void storeStatusBarNotification(StatusBarNotification sbn) {
+        Notification n = sbn.getNotification();
+        int notificationId = sbn.getId();
+        String packageName = sbn.getPackageName();
+        try {
+            Log.d(TAG, "querying for: " + String.valueOf(notificationId) + " " + packageName);
+            List<StoredNotification> notifications = getDao().queryBuilder().where()
+                                                               .eq("notification_id", notificationId)
+                                                               .and()
+                                                               .eq("package_name", packageName).query();
+            if (notifications.isEmpty()) {
+                String title = n.tickerText == null ? null : n.tickerText.toString();
+                getDao().create(new StoredNotification(notificationId, packageName, title, null));
+                Log.d(TAG, "stored notification! #" + notificationId + " " + title);
+            } else {
+                Log.d(TAG, "found notifications: ");
+                for(StoredNotification sn : notifications) {
+                    Log.d(TAG, sn.toString() + " " + "dismissed: " + String.valueOf(sn.getDismissed()));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void onNotificationRemoved(StatusBarNotification sbn) {
-        Log.i(TAG, "Removed ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
+        Log.i(TAG, "Removed #" + sbn.getId() + " " + sbn.getNotification().tickerText + " " + sbn.getPackageName());
     }
 
     private DatabaseHelper getDbHelper() {
